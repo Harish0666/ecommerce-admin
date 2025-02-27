dfd0c3c908e8416d1f60a8adf63f21481ffcb3bd
Harish0666 <hdaroch916@gmail.com>
2025-02-27 22:09:18 +0530

    new commit

--- src/apiService.js
+++ src/apiService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const apiClient = axios.create({
   baseURL: "http://localhost:8000",
   headers: {
-    "Content-Type": "application/json hahahaha",
+    "Content-Type": "application/json",
   },
 });
 
